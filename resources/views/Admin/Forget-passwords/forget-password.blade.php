1a54aa3535309fa108b7efb3dede731cdb2034a6
mohitmkg65 <mohitmkg65@gmail.com>
2023-12-29 18:02:46 +0530

    some chnages

--- resources/views/Admin/Forget-passwords/forget-password.blade.php
+++ resources/views/Admin/Forget-passwords/forget-password.blade.php
@@ -8,24 +8,24 @@
     <meta content="" name="author" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <!-- App favicon -->
-    <link rel="shortcut icon" href="{{URL::asset('ims_assets/images/construction_inventory.png')}}">
+    <link rel="shortcut icon" href="{{URL::asset('package_assets/images/construction_inventory.png')}}">
 
     <!-- third party css -->
-    <link href="{{URL::asset('ims_assets/libs/datatables.net-bs5/css/dataTables.bootstrap5.min.css')}}" rel="stylesheet" type="text/css" />
-    <link href="{{URL::asset('ims_assets/libs/datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css')}}" rel="stylesheet" type="text/css" />
-    <link href="{{URL::asset('ims_assets/libs/datatables.net-buttons-bs5/css/buttons.bootstrap5.min.css')}}" rel="stylesheet" type="text/css" />
-    <link href="{{URL::asset('ims_assets/libs/datatables.net-select-bs5/css/select.bootstrap5.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{URL::asset('package_assets/libs/datatables.net-bs5/css/dataTables.bootstrap5.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{URL::asset('package_assets/libs/datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{URL::asset('package_assets/libs/datatables.net-buttons-bs5/css/buttons.bootstrap5.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{URL::asset('package_assets/libs/datatables.net-select-bs5/css/select.bootstrap5.min.css')}}" rel="stylesheet" type="text/css" />
     <!-- third party css end -->
 
     <!-- App css -->
-    <link rel="stylesheet" href="{{URL::asset('ims_assets/css/app.min.css')}}"/>
-    <link rel="stylesheet" href="{{URL::asset('ims_assets/css/style.css')}}"/>
+    <link rel="stylesheet" href="{{URL::asset('package_assets/css/app.min.css')}}"/>
+    <link rel="stylesheet" href="{{URL::asset('package_assets/css/style.css')}}"/>
 
     <!-- icons -->
-    <link rel="stylesheet" href="{{URL::asset('ims_assets/css/icons.min.css')}}" />
+    <link rel="stylesheet" href="{{URL::asset('package_assets/css/icons.min.css')}}" />
 
     <!-- Toastr Css -->
-    <link rel="stylesheet" href="{{URL::asset('ims_assets/libs/toastr/build/toastr.min.css')}}" />
+    <link rel="stylesheet" href="{{URL::asset('package_assets/libs/toastr/build/toastr.min.css')}}" />
 
 </head>
 
@@ -36,7 +36,7 @@
                 <div class="col-md-8 col-lg-6 col-xl-4">
                     <div class="text-center">
                         <a href="">
-                            <img src="{{URL::asset('ims_assets/images/construction_inventory.png')}}" alt="" height="25" class="mx-auto">
+                            <img src="{{URL::asset('package_assets/images/construction_inventory.png')}}" alt="" height="25" class="mx-auto">
                         </a>
                         <p class="text-muted mt-2 mb-2">Constuction Inventory</p>
                     </div>
@@ -70,19 +70,19 @@
     </div>
 
     <!-- Vendor -->
-    <script src="{{ URL::asset('ims_assets/libs/jquery/jquery.min.js') }}"></script>
-    <script src="{{ URL::asset('ims_assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ URL::asset('ims_assets/libs/simplebar/simplebar.min.js') }}"></script>
-    <script src="{{ URL::asset('ims_assets/libs/node-waves/waves.min.js') }}"></script>
-    <script src="{{ URL::asset('ims_assets/libs/waypoints/lib/jquery.waypoints.min.js') }}"></script>
-    <script src="{{ URL::asset('ims_assets/libs/jquery.counterup/jquery.counterup.min.js') }}"></script>
-    <script src="{{ URL::asset('ims_assets/libs/feather-icons/feather.min.js') }}"></script>
+    <script src="{{ URL::asset('package_assets/libs/jquery/jquery.min.js') }}"></script>
+    <script src="{{ URL::asset('package_assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ URL::asset('package_assets/libs/simplebar/simplebar.min.js') }}"></script>
+    <script src="{{ URL::asset('package_assets/libs/node-waves/waves.min.js') }}"></script>
+    <script src="{{ URL::asset('package_assets/libs/waypoints/lib/jquery.waypoints.min.js') }}"></script>
+    <script src="{{ URL::asset('package_assets/libs/jquery.counterup/jquery.counterup.min.js') }}"></script>
+    <script src="{{ URL::asset('package_assets/libs/feather-icons/feather.min.js') }}"></script>
 
     <!-- App js -->
-    <script src="{{ URL::asset('ims_assets/js/app.min.js') }}"></script>
+    <script src="{{ URL::asset('package_assets/js/app.min.js') }}"></script>
 
     <!-- Toaster Js -->
-    <script src="{{ URL::asset('ims_assets/libs/toastr/build/toastr.min.js') }}"></script>
+    <script src="{{ URL::asset('package_assets/libs/toastr/build/toastr.min.js') }}"></script>
 
     <!-- Custom Js -->
     <script>
