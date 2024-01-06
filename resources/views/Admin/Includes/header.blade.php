a11d598be9bd2bc524083c6942e8a30c589b11bb
mohitmkg65 <mohitmkg65@gmail.com>
2024-01-06 17:08:54 +0530

    some changes

--- resources/views/Admin/Includes/header.blade.php
+++ resources/views/Admin/Includes/header.blade.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8" />
-    <title>@yield('meta_title')</title>
+    <title>@yield('meta_title')</title> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="" name="description" />
     <meta content="" name="author" />
